4ee1dca82a194837cc8f1594495d9088f6e11cd9
ayushgupta <ayushgupta7079@gmail.com>
2023-06-19 11:58:58 +0530

    Added generic_header_rubics Model

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <bits/stdc++.h>
+#include "RbiksCube3dArray.cpp"
 using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    RubiksCube3dArray objects3d;
+    objects3d.print();
+    objects3d.b();
+    objects3d.print();
+//    objects3d.bPrime();
+//    objects3d.bPrime();
+//    objects3d.print();
+
+
+//    cout<<objects3d.getColor( ,1 , 3);
 
 }
